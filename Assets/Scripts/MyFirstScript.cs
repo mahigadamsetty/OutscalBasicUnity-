f1563d5eb52d6ae542438aebb88c58c6c57d3da0
LAPTOP-RABA9COO\mahig <mahigadamsetty@gmail.com>
2021-03-13 20:44:15 +0530

    First commit

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MyFirstScript : MonoBehaviour
+{
+    public Button button;
+    public string newScene;
+    // Start is called before the first frame update
+     private void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+
+    // Update is called once per frame
+  private void OnButtonClick()
+    {
+        Debug.Log("Buuton click");
+        SceneManager.LoadScene(newScene);
+    }
+}
